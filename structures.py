0e10968cf7f2ff7b5dee8e187dadc58920ad5b66
nilsbenedikt <benediktnilsb@gmail.com>
2024-04-26 10:45:46 +0200

    add function to find outlier

--- structures.py
+++ structures.py
@@ -32,12 +32,14 @@ def calc_Q_and_Delta_fields(start,
         Q = 0.5*(np.linalg.norm(Omega, axis=(0,1))**2
              -np.linalg.norm(S, axis=(0,1))**2)
         
+        Delta = 27/4*np.linalg.det(G.transpose((2,3,4,0,1)))**2+Q**3
+        
         # Alternative
-        SijSjkSki = np.einsum('ijlmn,jklmn,kilmn->lmn',S,S,S)
+        '''SijSjkSki = np.einsum('ijlmn,jklmn,kilmn->lmn',S,S,S)
         WijWjkSki = np.einsum('ijlmn,jklmn,kilmn->lmn',Omega,Omega,S)
         R = -1/3*(SijSjkSki+3*WijWjkSki)
         
-        Delta = 27/4*R**2+Q**3
+        Delta = 27/4*R**2+Q**3'''
     
         normdata.write_Q_Delta(ii, Q, Delta)
         
@@ -89,10 +91,36 @@ def calc_std_Delta_by_variances(start,
     std_Delta_plus = std_Delta*(normdata.ny/(normdata.vtau**2))**6
     
     stdD = h5py.File(file_Q_Delta+f'.{start}_{end}_{step}.h5.stdD_var', 'w')
-    stdD.create_dataset('stdD', data=std_Delta_plus)    
+    stdD.create_dataset('stdD', data=std_Delta_plus)
+
+
+
+def find_Delta_outlier(start,
+                       end,
+                       step,
+                       file_read='./P125_21pi_vu/P125_21pi_vu',
+                       file_grad='./P125_21pi_vu/grad/P125_21pi_vu',
+                       file_Q_Delta='./P125_21pi_vu/hunt_chong/P125_21pi_vu'):
     
+    normdata = gd.get_data_norm(file_read=file_read,
+                                file_grad=file_grad,
+                                file_Q_Delta=file_Q_Delta)
+    normdata.geom_param(start,1,1,1)
     
+    max_Delta = 0
+    ii_max = 0
     
+    for ii in tqdm(range(start, end, step)):
+        Delta = normdata.read_chong_Delta_matrix(ii)
+        if np.max(Delta) > max_Delta:
+            max_Delta = np.max(Delta)
+            ii_max = ii
+    
+        
+    print(max_Delta, ii_max)
+        
+        
+        
 def calc_std_Q_plus(start,
                     end,
                     step,
