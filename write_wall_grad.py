563b2114447e9ad47d18f1d550a19428a0030550
nilsbenedikt <benediktnilsb@gmail.com>
2024-04-30 10:08:23 +0200

    for 2D cf fields

---
+++ write_wall_grad.py
@@ -0,0 +1,22 @@
+import h5py 
+import numpy as np
+from tqdm import tqdm
+
+
+path = './P125_21pi_vu/grad/P125_21pi_vu'
+
+start = 4544
+end = 4555
+step = 1
+
+for ii in tqdm(range(start, end, step)):
+    file = h5py.File(path+f'.{ii}.grad', 'r+')
+    dudy = np.array(file['dudy'])
+    file.close()
+    file2 = h5py.File(path+f'.{ii}.dudy', 'w')
+    dudy_wall = np.zeros((2,dudy.shape[1],dudy.shape[2]))
+    dudy_wall[0,:,:] = dudy[0,:,:]
+    dudy_wall[1,:,:] = dudy[-1,:,:]
+    file2.create_dataset('dudy', data=dudy_wall)
+    file2.close()
+    
