0b187789500334ef52c23ce86e3c572de92d3f39
nilsbenedikt <benediktnilsb@gmail.com>
2024-05-02 14:09:54 +0200

    corroboration with mean

--- write_wall_grad.py
+++ write_wall_grad.py
@@ -16,16 +16,23 @@ step = 1
 ny = 0.0004761904761904762
 U_bulk = 0.8988189365467902
 
+c_f_mean = np.zeros((1, int(np.floor((end-start)/step)+1)))
+
 for ii in tqdm(range(start, end, step)):
-    file = h5py.File(path_grad+f'.{ii}.grad', 'r+')
-    dudy = np.array(file['dudy'])
-    file.close()
-    file2 = h5py.File(path_cf+f'.{ii}.cf', 'w')
-    dudy_wall = np.zeros((2,dudy.shape[1],dudy.shape[2]))
-    dudy_wall[0,:,:] = dudy[0,:,:]
-    dudy_wall[1,:,:] = -dudy[-1,:,:]
+    # file = h5py.File(path_grad+f'.{ii}.grad', 'r+')
+    # dudy = np.array(file['dudy'])
+    # file.close()
+    file2 = h5py.File(path_cf+f'.{ii}.cf', 'r+')
+    # dudy_wall = np.zeros((2,dudy.shape[1],dudy.shape[2]))
+    # dudy_wall[0,:,:] = dudy[0,:,:]
+    # dudy_wall[1,:,:] = -dudy[-1,:,:]
     
-    c_f = 2*ny*dudy_wall/(U_bulk**2)
-    file2.create_dataset('c_f', data=c_f)
-    file2.close()
+    # c_f = 2*ny*dudy_wall/(U_bulk**2)
+    # file2.create_dataset('c_f', data=c_f)
+    # file2.close()
+    
+    c_f = np.array(file2['c_f'])
+    c_f_mean[0, int(np.floor((ii-start)/step))] = np.mean(c_f)
+    
+print(np.mean(c_f_mean))
     
