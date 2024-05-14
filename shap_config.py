0ad058c00c7fa7a103e6e79a38e88ca66337132d
nilsbenedikt <benediktnilsb@gmail.com>
2024-05-14 10:07:43 +0200

    new speed up attempt

--- shap_config.py
+++ shap_config.py
@@ -193,7 +193,7 @@ class shap_conf():
         for ii in range(num_struc):
             indx = np.array(np.where(self.segmentation == ii)).transpose()
             struc_indx.append(indx.astype(int))
-        self.struc_indx = np.array(struc_indx)
+        self.struc_indx = struc_indx
         
         
     def mask_dom(self,zs):
@@ -217,6 +217,7 @@ class shap_conf():
                     mask_out_correct[self.segmentation == jj,:] = self.background[self.segmentation == jj,:]        
         time3 = time()
         struc_selected = np.where(zs==0)[0].astype(int)
+        struc_selected = struc_selected.reshape([-1,1])
         print(struc_selected.shape)
         # indx = np.array(np.where(self.segmentation[..., np.newaxis] == struc_selected)[:3]).transpose()
         indx = np.vstack([self.struc_indx[struc_selected]])
