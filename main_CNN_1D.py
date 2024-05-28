f74cda5ca0179a875b08a8a205adee293bae40f7
nils <nils@turbulence3>
2024-05-28 18:08:55 +0200

    updated training

--- main_CNN_1D.py
+++ main_CNN_1D.py
@@ -13,14 +13,14 @@ import get_data_fun as gd
 
 CNN = ann.convolutional_residual(ngpu=1,
                                  fileddbb='/data/cremades/P125_21pi_vu/P125_21pi_vu',
-                                 file_cf='/media/nils/Elements/P125_21pi_vu_cf/P125_21pi_vu')#
+                                 file_cf='/data2/nils/P125_21pi_vu_cf/P125_21pi_vu')#
 dy = 1
 dz = 1
 dx = 1
 shpy = int((201-1)/dy)+1
 shpz = int((96-1)/dz)+1
 shpx = int((192-1)/dx)+1
-CNN.define_model(shp=(shpy,shpz,shpx,3),learat=1e-2) 
+CNN.define_model(shp=(shpy,shpz,shpx,3),learat=1e-4) 
 CNN.train_model(1000,7000,delta_t=100,delta_e=20,max_epoch=80,
                 batch_size=16,down_y=dy,down_z=dz,down_x=dx) #,fileddbb='../P125_21pi_vu/P125_21pir2') # 1000,7000
 
