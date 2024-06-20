929170b2ff673b7a3371d8e4de55cc6c9dbf1233
nilsbenedikt <benediktnilsb@gmail.com>
2024-06-20 13:00:32 +0200

    x axis

--- shap_config.py
+++ shap_config.py
@@ -3982,8 +3982,8 @@ class shap_conf():
                 plt.yticks([0.5,1])
                 plt.ylim([0,1.5])
             elif switch == 'vv':
-                plt.xticks([1,2])
-                plt.xlim([0,4])
+                plt.xticks([0.5,1,1.5])
+                plt.xlim([0,2])
                 plt.yticks([0.5,1,1.5])
                 plt.ylim([0,2])
             
