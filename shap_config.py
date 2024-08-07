bf81998e213ce64a2b80cba357047f12485e407e
nilsbenedikt <benediktnilsb@gmail.com>
2024-08-07 09:18:46 +0200

    axis limits

--- shap_config.py
+++ shap_config.py
@@ -2647,18 +2647,17 @@ class shap_conf():
                 
         plt.legend(handles,labels,fontsize=fs-4,loc='center left', bbox_to_anchor=(1, 0.5))
         if mode == 'mse':
-            # plt.tight_layout(rect=(0.5,0,1,1)) #
+            plt.tight_layout() #rect=(0.5,0,1,1)
             plt.xticks([1,2,3])
             plt.xlim([0,3.5])
             plt.yticks([2,4,6])
             plt.ylim([0,8])
-            plt.tight_layout()
         elif mode == 'cf':
-            plt.tight_layout() 
             plt.xticks([1,2,3,4,5,6,7,8,9])
             plt.xlim([0,3.5])
             plt.yticks([2,4,6])
             plt.ylim([0,8])
+            plt.tight_layout() 
         plt.savefig('hist2d_interp_vol_SHAP_'+colormap+str(structures)+'_30+.png')
         
         
